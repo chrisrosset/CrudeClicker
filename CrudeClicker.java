2b9103d10564585cf3f6d418a373d7782ba69daa
Christopher Rosset <rqg@rqg-base.(none)>
2011-01-22 23:57:55 +0000

    generally decreased clutter

--- CrudeClicker.java
+++ CrudeClicker.java
@@ -4,7 +4,7 @@ import java.awt.event.InputEvent;
 
 /**
  * click click click :>
- * @author rqg
+ * @author Krzysztof Rosset
  */
 public class CrudeClicker {
 
@@ -12,7 +12,7 @@ public class CrudeClicker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         if(args.length != 6) {
             System.out.println("CrudeClicker - usage:");
             System.out.println("CrudeClicker -x [num] -y [num] -c [num]");
@@ -20,40 +20,37 @@ public class CrudeClicker {
             System.out.println("-y - desktop y coordinate");
             System.out.println("-c - click count");
             return;
-        } else {
-            if(args[0].equals("-x") &&
-               args[1].matches("[0-9]+") && args[1].length() < 5 &&
-               args[2].equals("-y") &&     
-               args[3].matches("[0-9]+") && args[3].length() < 5 &&
-               args[4].equals("-c") &&
-               args[5].matches("[0-9]+") && args[5].length() < 5) {
-                
-                try {
-                    int x = Integer.parseInt(args[1]);
-                    int y = Integer.parseInt(args[3]);
-                    int c = Integer.parseInt(args[5]);
-                
-                    try {
-                        Robot clicker = new Robot();
-
-                        clicker.mouseMove(x, y);
-                        
-                        for(int i = 0; i < c; i++) {
-                            clicker.mousePress(InputEvent.BUTTON1_MASK);
-                            clicker.mouseRelease(InputEvent.BUTTON1_MASK);
-                        }
-                    } catch (AWTException ex) {
-                        System.out.println(1);
-                        return;
-                    }                                
-                } catch(Exception x) {
-                    System.out.println(0);
-                    return;
-                }                                                                
-            } else {
-                System.out.println(2);
-                return;
-            }
         }
-    }
+
+		if(args[0].equals("-x") &&
+		   args[1].matches("[0-9]+") && args[1].length() < 5 &&
+		   args[2].equals("-y") &&
+		   args[3].matches("[0-9]+") && args[3].length() < 5 &&
+		   args[4].equals("-c") &&
+		   args[5].matches("[0-9]+") && args[5].length() < 5) {
+
+			try {
+				int x = Integer.parseInt(args[1]);
+				int y = Integer.parseInt(args[3]);
+				int c = Integer.parseInt(args[5]);
+
+				Robot clicker = new Robot();
+				clicker.mouseMove(x, y);
+
+				for(int i = 0; i < c; i++) {
+					clicker.mousePress(InputEvent.BUTTON1_MASK);
+					clicker.mouseRelease(InputEvent.BUTTON1_MASK);
+				}
+			} catch (AWTException ex) {
+				System.out.println("AWT exception thrown.");
+				return;
+			} catch(Exception x) {
+				System.out.println("Exception thrown.");
+				return;
+			}
+		} else {
+			System.out.println("Incorrect argument format.");
+			return;
+		}
+	}
 }
